5a517280c7fca88a19ec957ffc18d56ab03ab264
Oleg Frey <olfrey@ya.ru>
2019-07-20 12:10:17 +0300

    desktop

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,26 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+
+	var sliderList = $('.slider__list'),
+			sliderNav = $('.slider__nav');
+
+	sliderList.slick({
+		centerMode: true,
+		arrows: false,
+		variableWidth: true,
+		asNavFor: sliderNav,
+		infinite: false,
+		initialSlide: 3
+	})
+
+	sliderNav.slick({
+		centerMode: true,
+		arrows: false,
+		variableWidth: true,
+		asNavFor: sliderList,
+		infinite: false,
+		focusOnSelect: true,
+		initialSlide: 3
+	})
 });
